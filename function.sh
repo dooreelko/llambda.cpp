0786dcf3a2658efbb27c2474940c3c1e4855830f
dooreelko <sascha.fedorenko@pm.me>
2023-08-23 15:20:38 +0200

    kind of auth

--- function.sh
+++ function.sh
@@ -2,18 +2,31 @@
 
 set -exu
 
+LLAMA_SECRET=${LLAMA_SECRET:-}
 LLAMA_EXTRA_ARGS=${LLAMA_EXTRA_ARGS:-}
 
 function handler () {
   EVENT_DATA=$1
+
+  APIKEY=$(echo "$EVENT_DATA" | jq -r '.["api-key"]')
+
+  if [ -z "$LLAMA_SECRET" ]; then 
+    echo '{"errorMessage" : "Missing API secret in LLAMA_SECRET.", "errorType" : "ServerError"}'
+    exit 
+  fi
+
+  if [ "$APIKEY" != "$LLAMA_SECRET" ]; then 
+    echo '{"errorMessage" : "Wrong or missing API key.", "errorType" : "Unauthorised"}'
+    exit 
+  fi
 
   NGEN=$(echo "$EVENT_DATA" | jq -r '.["n-generate"]')
   NGEN=${NGEN:-10}
 
   PROMPT=$(echo "$EVENT_DATA" | jq -r '.prompt')
   if [ -z "$PROMPT" ]; then
-    echo "Missing prompt in payload"
-    exit 1
+    echo '{"errorMessage" : "Missing prompt in payload.", "errorType" : "BadRequest"}'
+    exit 
   fi
 
   "$LLAMA_BIN" $LLAMA_EXTRA_ARGS -n "$NGEN" -m "$LLAMA_LOCAL_MODEL" -p "$PROMPT" | tee /dev/stderr
